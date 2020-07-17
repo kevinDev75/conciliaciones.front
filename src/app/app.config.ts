a2eab3283913b8976b8a0ce3e189a1d4e8c75a84
Kevin Cordova <kevin.cordova@materiagris.pe>
2020-07-17 12:52:02 -0500

    config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,11 +5,11 @@ import { Injectable } from "@angular/core";
 })
 export class AppConfig {
   //public readonly apiUrl = 'http://localhost:2086/';
-  // public readonly apiUrl = 'http://localhost:2086';
+   public readonly apiUrl = 'http://localhost:2086';
 
-  public readonly apiUrl = "http://10.10.1.56/conciliacionesBancaria";
+  //public readonly apiUrl = "http://10.10.1.56/conciliacionesBancaria";
 
-  // public readonly apiUrl = 'http://10.10.1.56/ConciliacionRESTDESA';
+  // public readonly apiUrl = 'http://1033.10.1.56/ConciliacionRESTDESA';
   // public readonly apiUrl = 'http://190.216.170.173/ConciliacionRESTDESA';
   // public readonly apiUrl = 'http://172.23.2.104/ConciliacionRESTPROD'; // PRODUCCIÓN
 }
